fbf5850ae45de4662304d8395654521a7d22b149
jessesbyers <jesse.s.byers@gmail.com>
2020-03-27 16:01:06 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -34,8 +34,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
+  return { orangePeel: state.items }
 }
 
 export default connect(mapStateToProps)(App);
