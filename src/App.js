c1d189251854c322c3ad83da498f83b8032b1d64
mani21na <mani21na@gmail.com>
2020-04-18 23:02:45 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ class App extends Component {
       type: 'GET_COUNT_OF_USERS',
     });
   }
-
+ 
   render() {
-    // debugger;
+    debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -34,8 +34,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
   return { items: state.items }
 }
-
+ 
 export default connect(mapStateToProps)(App);
